7ba98861bc834f32daebaa6d7b131b175fe9c26d
Bruno Tostes <btfjulio@hotmail.com>
2019-11-19 13:11:21 -0300

    Updates

---
+++ precache-manifest.cc0a7921c9b60bb262cc75273f461509.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ada3343df0f69382c091cb09e68cd453",
+    "url": "/monster-react/index.html"
+  },
+  {
+    "revision": "6e86725e5e79cf679660",
+    "url": "/monster-react/static/css/main.f3fd2796.chunk.css"
+  },
+  {
+    "revision": "f4fcda3c91ee4aeb99d8",
+    "url": "/monster-react/static/js/2.2c93c52a.chunk.js"
+  },
+  {
+    "revision": "6e86725e5e79cf679660",
+    "url": "/monster-react/static/js/main.e5d68d22.chunk.js"
+  },
+  {
+    "revision": "cf4cea2ba909104a9fc3",
+    "url": "/monster-react/static/js/runtime-main.3575b566.js"
+  }
+]);
